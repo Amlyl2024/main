8a85f3262c346915242ca5e367ef6c2d8d49ad60
Amlyl2024 <mahdi-fla@hotmail.fr>
2025-02-11 19:14:44 +0100

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Logo({ className = "h-8 w-8" }: { className?: string }) {
   return (
     <img 
-      src="https://fevtmhcvpjxpdqlgoxvt.supabase.co/storage/v1/object/public/List%20of%20projects/logo%20PNG.png" 
+      src="https://ortealnkwvhxrpbbhrpi.supabase.co/storage/v1/object/public/project-images/bd4c532c-377d-41d0-a20f-28770651f0d9/0.062106184467645376.png" 
       alt="AMLYL Logo" 
       className={className}
       style={{ objectFit: 'contain' }}
